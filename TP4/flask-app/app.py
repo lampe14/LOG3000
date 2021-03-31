12a97c0136a6422a558f02101dc4e459078a21d8
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-03-31 15:22:50 -0400

    added remise tp4

---
+++ TP4/flask-app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+# list of cat images
+images = [ "https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif",
+"https://media.giphy.com/media/xUPGcyi4YxcZp8dWZq/giphy.gif",
+"https://media.giphy.com/media/1iu8uG2cjYFZS6wTxv/giphy.gif",
+"https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
+"https://media.giphy.com/media/lJNoBCvQYp7nq/giphy.gif"
+]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
